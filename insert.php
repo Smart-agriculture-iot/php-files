6aa239cbaebdf1994d946e61b48d6ece78750875
unknown <angelrwabu@gmail.com>
2022-05-31 18:16:58 +0200

    php

---
+++ insert.php
@@ -0,0 +1,28 @@
+﻿<?php
+if(isset($_GET["temperature"]) && isset($_GET["humidity"])&& isset($_GET["soilmoisture"])) {
+   $temperature = $_GET["temperature"]; // get temperature value from HTTP GET
+   $humidity = $_GET["humidity"]; // get humidity value from HTTP GET
+   $soilmoisture = $_GET["soilmoisture"]; // get water value from HTTP GET
+   $servername = "localhost";
+   $username = "root";
+   $password = "";
+   $dbname = "project";
+
+   // Create connection
+   $conn = new mysqli($servername, $username, $password, $dbname);
+   // Check connection
+   if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+   }
+   
+   $sql = "INSERT INTO sm (humidity,temperature,soilmoisture) VALUES ($temperature,$humidity,$soilmoisture)";
+   if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+   } else {
+      echo "Error: " . $sql . " => " . $conn->error;
+   }
+   $conn->close();
+} else {
+   echo "Humidity,Temperature and soilmoisture are not set";
+}
+?>
---
+++ value.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <title>UR project</title>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <?php
+    
+    $db = mysqli_connect("localhost","root","","project");
+    
+    if(!$db)
+    {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    ?>
+        <tbody>
+         <?php
+    
+    $records = mysqli_query($db,"select temperature,humidity,soilmoisture from sm  Order by id DESC LIMIT 1"); // fetch data from database
+	//"select temperature,humidity,soilmoisture from sm Order by id DESC; 
+    
+    while($data = mysqli_fetch_array($records))
+    {
+    ?>
+      
+	  <div class="container">
+	 <div id="Container">
+        
+        <div class="Card" id="Card1">
+            <div class="CardName">Temperature</div>
+            <div class="CardValue" id="Temp"><?php echo $data['temperature'];?>C</div>
+        </div>
+		 <div class="Card" id="Card2">
+            <div class="CardName">Humidity</div>
+            <div class="CardValue" id="Humidity"><?php echo $data['humidity'];?>%</div>
+        </div>
+		 <div class="Card" id="Card1">
+            <div class="CardName">Soil Moisture</div>
+            <div class="CardValue" id="Soilmoisture"><?php echo $data['soilmoisture'];?></div>
+        </div>
+		</div>
+    <?php
+ }
+    ?>
+    <?php mysqli_close($db); ?>
+    </body>
+    </html>
